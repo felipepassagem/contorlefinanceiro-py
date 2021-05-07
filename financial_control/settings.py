6df0757814c0260b3ed20b021ea15f8ee2356ed7
felipepassagem <64990930+felipepassagem@users.noreply.github.com>
2021-05-07 09:04:15 -0300

    Update settings.py

--- financial_control/settings.py
+++ financial_control/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*lfco27n=rtcgv)*x$kvq^kko3qgs5@pai+xv#dp8i2&+uzjp8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
